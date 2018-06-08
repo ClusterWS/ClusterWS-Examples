ef796cbd0a11595ca8cce2dabcfd5a9b30b86290
Dmitrii Goriunov <goriunovd@gmail.com>
2018-06-08 22:08:51 +1200

    Added pubsub guide

---
+++ pubsub_communication/server.js
@@ -0,0 +1,28 @@
+const ClusterWS = require('clusterws');
+const express = require('express');
+
+// Configure ClusterWS for more infor look at the bottom of https://github.com/ClusterWS/ClusterWS/wiki/Installation-and-Configuration
+const clusterws = new ClusterWS({
+  worker: Worker,
+  port: 3000
+});
+
+// Note that pub sub allows to communicate between all clients including from different workers and servers
+function Worker() {
+  const wss = this.wss;
+  const server = this.server;
+
+  const app = express();
+  app.use(express.static('public'));
+
+  server.on('request', app);
+
+  // Most of websocket messaging code will go in this block
+  wss.on('connection', (socket) => {
+    console.log('New socket is connected');
+    // Pubsub code usualy will be in front end. Check index.html for basic setup
+
+    // To publish messages from server to all users subscribe to the specific channel use
+    // wss.publish('channel name', 'your message')
+  });
+}
